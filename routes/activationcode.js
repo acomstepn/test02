c026c81501528b900cf9de5cb1f2dab6e9bf1d22
acomstepn <acomstepn@gmail.com>
2022-04-25 11:16:21 +0900

    使用済みアクティベーションコードのグレー表示

--- routes/activationcode.js
+++ routes/activationcode.js
@@ -9,11 +9,15 @@ const title = 'カードローン地獄の成れの果て';
 ActivationCode.sync();
 
 router.get('/', async (req, res, next) => {
-	const rows = await ActivationCode.findAll();
+	const rows = await ActivationCode.findAll({
+		order: [
+			['createdAt', 'DESC']
+		]
+	});
 	const items = [];
 	rows.forEach(row => {
 		const createdAt = moment(row.createdAt).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm');
-		items.unshift({code: row.code, createdAt: createdAt});
+		items.push({code: row.code, enable: row.enable, createdAt: createdAt});
 	});
 	res.render('activationcode', { title: title, items: items });
 });
