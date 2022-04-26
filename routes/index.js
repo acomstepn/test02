72da48c90275d2301bd03d5cc947bf73073e03c3
acomstepn <acomstepn@gmail.com>
2022-04-26 17:14:09 +0900

    小数点表示の誤差対処

--- routes/index.js
+++ routes/index.js
@@ -16,67 +16,93 @@ router.get('/', async (req, res, next) => {
 	const items = [];
 
 	rows.forEach(row => {
+		const n2s = (val) => {
+			let sign = '';
+			if (val < 0) {
+				val = -val;
+				sign = '-';
+			}
+			let v0 = val%1000000000;
+			const v1 = (val-v0)/1000000000%1000;
+			const v2 = (((val-v0)/1000000000)-v1)/1000%1000;
+			if (v0 === 0) {
+				if(v2 === 0) {
+					return `${sign}${v1}`;
+				} else {
+					return `${sign}${v2},${('00'+v1).slice(-3)}`;
+				}
+			} else {
+				let fig = 9;
+				while (v0%10 === 0) {
+					v0 /= 10;
+					fig--;
+				}
+				if(v2 === 0) {
+					return `${sign}${v1}.${('00000000'+v0).slice(-fig)}`;
+				} else {
+					return `d:${sign}${v2},${('00'+v1).slice(-3)}.${('00000000'+v0).slice(-fig)}`;
+				}
+			}
+		}
 		const id = row.id;
 		const kari_kamoku = row.kari_kamoku;
-		const kari_price = row.kari_price;
+		const kari_price = Math.round(row.kari_price*1000000000);
 		const kashi_kamoku = row.kashi_kamoku;
-		const kashi_price = row.kashi_price;
+		const kashi_price = Math.round(row.kashi_price*1000000000);
 		const tekiyo = row.tekiyo;
 		const createdAt = moment(row.createdAt).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm');
 		if (kari_kamoku === 'Spending GST') {
-			GST += kari_price*100;
+			GST += kari_price;
 		} else if (kari_kamoku === 'Spending GMT') {
-			GMT += kari_price*1000000;
+			GMT += kari_price;
 		} else if (kari_kamoku === 'Spending SOL') {
-			SOL += kari_price*1000000;
+			SOL += kari_price;
 		} else if (kari_kamoku === 'Wallet GST') {
-			WGST += kari_price*1000000000;
+			WGST += kari_price;
 		} else if (kari_kamoku === 'Wallet GMT') {
-			WGMT += kari_price*1000000;
+			WGMT += kari_price;
 		} else if (kari_kamoku === 'Wallet SOL') {
-			WSOL += kari_price*1000000000;
+			WSOL += kari_price;
 		} else if (kari_kamoku === 'Wallet USDC') {
-			USDC += kari_price*1;
+			USDC += kari_price;
 		} else if (kari_kamoku === '借入金') {
 			acom -= kari_price;
 		}
 		if (kashi_kamoku === 'Spending GST') {
-			GST -= kashi_price*100;
+			GST -= kashi_price;
 		} else if (kashi_kamoku === 'Spending GMT') {
-			GMT -= kashi_price*1000000;
+			GMT -= kashi_price;
 		} else if (kashi_kamoku === 'Spending SOL') {
-			SOL -= kashi_price*1000000;
+			SOL -= kashi_price;
 		} else if (kashi_kamoku === 'Wallet GST') {
-			WGST -= kashi_price*1000000000;
+			WGST -= kashi_price;
 		} else if (kashi_kamoku === 'Wallet GMT') {
-			WGMT -= kashi_price*1000000;
+			WGMT -= kashi_price;
 		} else if (kashi_kamoku === 'Wallet SOL') {
-			WSOL -= kashi_price*1000000000;
+			WSOL -= kashi_price;
 		} else if (kashi_kamoku === 'Wallet USDC') {
-			USDC -= kashi_price*1;
+			USDC -= kashi_price;
 		} else if (kashi_kamoku === '借入金') {
 			acom += kashi_price;
 		}
-		const text = `${GST/100} GST ${GMT/1000000} GMT ${SOL/1000000} SOL 負債 ${acom}`;
-		const wallet = '';
 		items.unshift({
 			id: id,
 			kari_kamoku: kari_kamoku,
-			kari_price: kari_price,
+			kari_price: row.kari_price,
 			kashi_kamoku: kashi_kamoku,
-			kashi_price: kashi_price,
+			kashi_price: row.kashi_price,
 			tekiyo: tekiyo,
 
-			gst: GST/100,
-			gmt: GMT/1000000,
-			sol: SOL/1000000,
+			gst: n2s(GST),
+			gmt: n2s(GMT),
+			sol: n2s(SOL),
 
-			wsol: WSOL/1000000000,
-			wgst: WGST/1000000000,
-			wgmt: WGMT/1000000,
-			usdc: USDC,
+			wsol: n2s(WSOL),
+			wgst: n2s(WGST),
+			wgmt: n2s(WGMT),
+			usdc: n2s(USDC),
 
-			acom: acom,
+			acom: n2s(acom),
 			createdAt: createdAt
 		});
 	});
