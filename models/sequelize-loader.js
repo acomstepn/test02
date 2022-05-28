712acb309eb5b6f6b8ffea54a98ff2154303acff
acomstepn <acomstepn@gmail.com>
2022-05-28 17:52:40 +0900

    sequelize timezone設定

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.STEPNDB_DB, process.env.STEPNDB_USER, process.env.STEPNDB_PASS, {
 		host: process.env.STEPNDB_HOST,
 		port: process.env.STEPNDB_PORT,
-		dialect: 'mysql'
+		dialect: 'mysql',
+		timezone: '+09:00'
 	}
 );
 
